4fe452854334bb21ba359db127f820ee130d43dd
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2024-03-02 15:17:08 -0700

    Interpreter Backend Completion Waiting for Integration

--- application/interpreter.py
+++ application/interpreter.py
@@ -42,17 +42,17 @@ def dataListCreation(data):
     data_list = np.delete(data_list, 0, 0)
 
     return data_list
-
 #------------ HELPER FUNCTIONS ------------
+
 #FUNCTION - singleInterpreter
 #Input - single day file (helium, metal, etc.)
 #Output - 2d array of MAP (cols: x,rows: y) with values
 #NOTES: Only interprets cols(?, x, y, value) from a dataset 
-def singleInterpreter(data_file_name, day):
+def singleInterpreter(data_file, day):
     global MAX_VALUE
     global MIN_VALUE
     file_dir = os.path.dirname(__file__)
-    file_name = os.path.join(file_dir, f'../data/{data_file_name}_data_day_{day}.csv')
+    file_name = os.path.join(file_dir, f'../data/{data_file}_data_day_{day}.csv')
    
    #Data List Creation of interpreted file
     with open(file_name) as data:
@@ -84,22 +84,41 @@ def singleInterpreter(data_file_name, day):
         if (land_water == 1):
             map[y_value][x_value] = -1
         else: 
-            if (map[y_value][x_value] > MAX_VALUE):
+            if (map[y_value][x_value] >= MAX_VALUE):
                 MAX_VALUE = map[y_value][x_value]
-            elif (map[y_value][x_value] < MIN_VALUE):
+            elif (map[y_value][x_value] <= MIN_VALUE):
                 MIN_VALUE = map[y_value][x_value]
 
     return map
 
 #FUNCTION - multiInterpreter
 #Input - input from main for which data is required for mapping
-#Output - 3D array of maps as (1 dimension (width) as total days, 2 dimensions of x,y map of values)
+#Output - 3D array of maps as (1 dimension (width) as total days, 2 dimensions of x,y map of normalized values W.R.T MIN MAX GLOBALS)
+#NOTES: ASSUMPTION of running 30 days 
+def multiInterpreter(data_file):
 
+    #Main Dataframe Creation
+    dataframe = np.zeros((30, 100, 100))
 
-#main
-#Input - 
+    #Day 1 - 30 Adder to Dataframe
+    for i in range(0, 30):
+        dataframe[i] = singleInterpreter(data_file, i + 1) 
+    
+    #Normalization of Values
+    difference = MAX_VALUE - MIN_VALUE
+    for i in range(0, len(dataframe)):
+        for j in range(0 , len(dataframe[i])):
+            for k in range(0, len(dataframe[i][j])):
+                value = dataframe[i][j][k]
+                if value != -1:
+                    dataframe[i][j][k] = (value - MIN_VALUE)/difference
+
+        plt.imshow(dataframe[i], cmap='hot', interpolation='nearest')
+        plt.show()
+    #return dataframe
+
 def main(input):
 
-    return
+    multiInterpreter(input)
 
-singleInterpreter("algal", 30)
+main("TO INTEGRATE WITH ALGORITHM")
