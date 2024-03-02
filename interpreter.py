2b84bd43374c0ee686d5dce12d9d11e2a22365c0
Syed Abraham Ahmed <syed.abraham14@gmail.com>
2024-03-02 11:42:54 -0700

    Initial Interpreter Commit

---
+++ interpreter.py
@@ -0,0 +1,16 @@
+import numpy as np
+import csv
+
+#FUNCTION - Interpreter
+#Input - single day file
+#Output - 2d array of rows:UNDEFINED | cols: x, y, value, normalized value W.R.T min/max
+#NOTES: Only interprets cols(?,x, y, value)
+def singleInterpreter(data_file_name):
+
+    return 0
+
+        
+
+
+
+#interpreter("data/algal_data_day_1.csv")
